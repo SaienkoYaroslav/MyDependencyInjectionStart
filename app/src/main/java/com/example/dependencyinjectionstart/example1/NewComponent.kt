1842fd8e30f75ec61f9786f3ea82e172801392c2
SaienkoYaroslav <saienko.yaroslav01@gmail.com>
2023-04-22 17:44:44 +0300

    dagger dependencies, inject, get and component

---
+++ app/src/main/java/com/example/dependencyinjectionstart/example1/NewComponent.kt
@@ -0,0 +1,18 @@
+package com.example.dependencyinjectionstart.example1
+
+import dagger.Component
+
+@Component
+interface NewComponent {
+
+    fun getKeyBoard(): Keyboard
+
+    fun getMouse(): Mouse
+
+    fun getMonitor(): Monitor
+
+    fun inject(activity: Activity)
+
+
+
+}
