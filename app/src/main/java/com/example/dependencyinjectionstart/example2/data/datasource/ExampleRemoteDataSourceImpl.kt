333ac13e5c2492eb37baa3c5993434d0c085e21d
SaienkoYaroslav <saienko.yaroslav01@gmail.com>
2023-05-09 16:46:18 +0300

    Qualifiers

--- app/src/main/java/com/example/dependencyinjectionstart/example2/data/datasource/ExampleRemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/data/datasource/ExampleRemoteDataSourceImpl.kt
@@ -1,5 +1,6 @@
 package com.example.dependencyinjectionstart.example2.data.datasource
 
+import android.util.Log
 import com.example.dependencyinjectionstart.example2.data.network.ExampleApiService
 import javax.inject.Inject
 
@@ -9,5 +10,6 @@ class ExampleRemoteDataSourceImpl @Inject constructor(
 
     override fun method() {
         apiService.method()
+        Log.d("RemoteDataSource", "WORK")
     }
 }
--- app/src/main/java/com/example/dependencyinjectionstart/example2/data/repository/ExampleRepositoryImpl.kt
+++ app/src/main/java/com/example/dependencyinjectionstart/example2/data/repository/ExampleRepositoryImpl.kt
@@ -3,12 +3,15 @@ package com.example.dependencyinjectionstart.example2.data.repository
 import com.example.dependencyinjectionstart.example2.data.datasource.ExampleLocalDataSource
 import com.example.dependencyinjectionstart.example2.data.mapper.ExampleMapper
 import com.example.dependencyinjectionstart.example2.data.datasource.ExampleRemoteDataSource
+import com.example.dependencyinjectionstart.example2.di.TestRemoteDataQualifier
+import com.example.dependencyinjectionstart.example2.di.WorkRemoteDataQualifier
 import com.example.dependencyinjectionstart.example2.domain.ExampleRepository
 import javax.inject.Inject
+import javax.inject.Named
 
 class ExampleRepositoryImpl @Inject constructor(
     private val localDataSource: ExampleLocalDataSource,
-    private val remoteDataSource: ExampleRemoteDataSource,
+    @WorkRemoteDataQualifier private val remoteDataSource: ExampleRemoteDataSource,
     private val mapper: ExampleMapper
 ) : ExampleRepository {
 
